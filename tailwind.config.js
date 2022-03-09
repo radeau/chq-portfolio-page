ffeff57ceb23d65baa694f7e87aba226aa579c0c
radeau <kevinradp@gmail.com>
2022-03-10 02:47:34 +0400

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'chqolor': {
+          50: '#f3f3f4',
+          100: '#776C6C',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+      }
+    }
+  },
+  plugins: [],
+}
